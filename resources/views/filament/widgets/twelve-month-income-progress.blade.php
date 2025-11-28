f0acb16ef81c299c23fce82b339e9c5c96a05830
stefanrakic <stefan.rakic@swiftylabs.io>
2025-11-28 11:08:27 +0100

    Setup widgets

--- resources/views/filament/widgets/twelve-month-income-progress.blade.php
+++ resources/views/filament/widgets/twelve-month-income-progress.blade.php
@@ -24,11 +24,11 @@
             <div>
                 <div class="flex justify-between text-sm mb-2">
                     <span class="font-medium">Ostvareno: {{ number_format($twelveMonthIncome, 0, ',', '.') }} RSD</span>
-                    <span class="font-medium">{{ number_format($percentageUsed, 1) }}%</span>
+                    <span class="font-medium text-blue-600">{{ number_format($percentageUsed, 1) }}%</span>
                 </div>
-                <div class="w-full bg-gray-200 rounded-full h-8 dark:bg-gray-700 overflow-hidden">
-                    <div class="h-8 rounded-full transition-all duration-500 flex items-center justify-end pr-3"
-                         style="width: {{ min($percentageUsed, 100) }}%; background-color: rgb(59, 130, 246);">
+                <div class="relative w-full rounded-full" style="background-color: #f8fafc; height: 32px;">
+                    <div class="absolute top-0 left-0 rounded-full transition-all duration-500 flex items-center justify-end pr-3"
+                         style="width: {{ min($percentageUsed, 100) }}%; background-color: rgb(59, 130, 246); height: 32px;">
                         @if($percentageUsed > 5)
                             <span class="text-sm font-semibold text-white">{{ number_format($percentageUsed, 1) }}%</span>
                         @endif
